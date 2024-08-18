0e96db4e64d54d0985b97f54ea5957fa034f43d4
arafatrased <rasedmathku@gmail.com>
2024-08-18 23:59:22 +0600

    Header banner and women section done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily: {
+      'bebas' : ["Bebas Neue", "sans-serif"],
+      'roboto' : ["Roboto", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
